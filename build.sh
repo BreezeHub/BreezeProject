623b1a937102e8aa2a2bca1e8cfd744083f1a34e
carlton355 <carlton.pringle@gmail.com>
2017-07-20 09:09:17 +0700

    removed build.sh added sln to travis.yml

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dotnet --info
-echo STARTED dotnet restore
-dotnet restore -v m
-echo STARTED dotnet build
-dotnet build -c Release ${path} -v m
-echo STARTED dotnet test
-dotnet test -c Release ./BreezeCommon.Tests/BreezeCommon.Tests.csproj -v m
