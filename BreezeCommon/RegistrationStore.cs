b510f2acefd8e5e9df5d40d7c5ad45c4efcd3bdd
carlton355 <carlton.pringle@gmail.com>
2017-10-07 13:31:37 +0700

    updated RegistrationStore with locks around file access.

--- BreezeCommon/RegistrationStore.cs
+++ BreezeCommon/RegistrationStore.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -10,101 +10,107 @@ using NBitcoin.Protocol;
 
 namespace BreezeCommon
 {
-    public class RegistrationStore : IRegistrationStore
-    {
-        string StorePath;
+	public class RegistrationStore : IRegistrationStore
+	{
+		string StorePath;
 
-        public RegistrationStore(string storePath)
-        {
-            StorePath = storePath;
-        }
+		private static object lock_object = new object();
 
-        public RegistrationStore(Network network)
-        {
-            StorePath = GetRegistrationStoreFilePath(network);
-        }
+		public RegistrationStore(string storePath)
+		{
+			StorePath = storePath;
+		}
 
-        public string Name { get; } = "RegistrationStore";
-        public string StoreFileName { get; } = "registrationHistory.json";
+		public RegistrationStore(Network network)
+		{
+			StorePath = GetRegistrationStoreFilePath(network);
+		}
 
-        public bool Add(RegistrationRecord regRecord)
-        {
-            List<RegistrationRecord> registrations = GetRecordsOrCreateFile();
+		public string Name { get; } = "RegistrationStore";
+		public string StoreFileName { get; } = "registrationHistory.json";
 
-            registrations.Add(regRecord);
+		public bool Add(RegistrationRecord regRecord)
+		{
+			lock (RegistrationStore.lock_object)
+			{
+				List<RegistrationRecord> registrations = GetRecordsOrCreateFile();
 
-			//JsonSerializerSettings settings = new JsonSerializerSettings();
-			//settings.Converters.Add(new IPAddressConverter());
-			//settings.Formatting = Formatting.Indented;
+				registrations.Add(regRecord);
 
-			//JsonSerializerSettings isoDateFormatSettings = new JsonSerializerSettings
-            //{
-            //    DateFormatHandling = DateFormatHandling.IsoDateFormat
-            //};
+				//JsonSerializerSettings settings = new JsonSerializerSettings();
+				//settings.Converters.Add(new IPAddressConverter());
+				//settings.Formatting = Formatting.Indented;
 
-            string regJson = JsonConvert.SerializeObject(registrations);
-            File.WriteAllText(StorePath, regJson);
+				//JsonSerializerSettings isoDateFormatSettings = new JsonSerializerSettings
+				//{
+				//    DateFormatHandling = DateFormatHandling.IsoDateFormat
+				//};
 
-            return true;
-        }
+				string regJson = JsonConvert.SerializeObject(registrations);
+				File.WriteAllText(StorePath, regJson);
 
-        public void AddCapsule(RegistrationCapsule capsule, Network network)
-        {
-            RegistrationToken token = new RegistrationToken();
-            token.ParseTransaction(capsule.RegistrationTransaction, network);
-            RegistrationRecord record = new RegistrationRecord(DateTime.Now,
-                Guid.NewGuid(),
-                capsule.RegistrationTransaction.GetHash().ToString(),
-                capsule.RegistrationTransaction.ToHex(),
-                token,
-                capsule.RegistrationTransactionProof);
+				return true;
+			}
+		}
 
-            Add(record);
-        }
+		public void AddCapsule(RegistrationCapsule capsule, Network network)
+		{
+			RegistrationToken token = new RegistrationToken();
+			token.ParseTransaction(capsule.RegistrationTransaction, network);
+			RegistrationRecord record = new RegistrationRecord(DateTime.Now,
+				Guid.NewGuid(),
+				capsule.RegistrationTransaction.GetHash().ToString(),
+				capsule.RegistrationTransaction.ToHex(),
+				token,
+				capsule.RegistrationTransactionProof);
+
+			Add(record);
+		}
 
 		public List<RegistrationRecord> GetByServerId(string serverId)
-        {
-            List<RegistrationRecord> registrations = GetRecordsOrCreateFile();
-            List<RegistrationRecord> filtered = new List<RegistrationRecord>();
-
-            foreach (RegistrationRecord record in registrations)
-            {
-                if (record.Record.ServerId == serverId)
-                {
-                    filtered.Add(record);
-                }
-            }
-
-            return filtered;
-        }
-
-		public List<RegistrationRecord> GetAll()
-        { 
-			return GetRecordsOrCreateFile();
-        }
-
-        public List<RegistrationCapsule> GetAllAsCapsules()
-        {
-            List<RegistrationCapsule> capsuleList = new List<RegistrationCapsule>();
-
-            foreach (RegistrationRecord record in GetRecordsOrCreateFile())
-            {
-                RegistrationCapsule tempCapsule = new RegistrationCapsule(record.RecordTxProof, Transaction.Parse(record.RecordTxHex));
-                capsuleList.Add(tempCapsule);
-            }
-
-            return capsuleList;
-        }
-
-        public RegistrationRecord GetByGuid(Guid guid)
 		{
-            List<RegistrationRecord> registrations = GetRecordsOrCreateFile();
+			List<RegistrationRecord> registrations = GetRecordsOrCreateFile();
+			List<RegistrationRecord> filtered = new List<RegistrationRecord>();
 
 			foreach (RegistrationRecord record in registrations)
 			{
-                if (record.RecordGuid == guid)
+				if (record.Record.ServerId == serverId)
 				{
-                    return record;
+					filtered.Add(record);
+				}
+			}
+
+			return filtered;
+		}
+
+		public List<RegistrationRecord> GetAll()
+		{
+			return GetRecordsOrCreateFile();
+		}
+
+		public List<RegistrationCapsule> GetAllAsCapsules()
+		{
+			List<RegistrationCapsule> capsuleList = new List<RegistrationCapsule>();
+
+			foreach (RegistrationRecord record in GetRecordsOrCreateFile())
+			{
+				RegistrationCapsule tempCapsule =
+					new RegistrationCapsule(record.RecordTxProof, Transaction.Parse(record.RecordTxHex));
+				capsuleList.Add(tempCapsule);
+			}
+
+			return capsuleList;
+		}
+
+		public RegistrationRecord GetByGuid(Guid guid)
+		{
+			List<RegistrationRecord> registrations = GetRecordsOrCreateFile();
+
+			foreach (RegistrationRecord record in registrations)
+			{
+				if (record.RecordGuid == guid)
+				{
+					return record;
 				}
 			}
 
@@ -113,78 +119,83 @@ namespace BreezeCommon
 
 		public bool Delete(Guid guid)
 		{
-            List<RegistrationRecord> registrations = GetRecordsOrCreateFile();
-            List<RegistrationRecord> modified = new List<RegistrationRecord>();
+			lock (RegistrationStore.lock_object)
+			{
+				List<RegistrationRecord> registrations = GetRecordsOrCreateFile();
+				List<RegistrationRecord> modified = new List<RegistrationRecord>();
 
-            foreach (RegistrationRecord record in registrations)
-            {
-                if (record.RecordGuid != guid)
-                    modified.Add(record);
-            }
+				foreach (RegistrationRecord record in registrations)
+				{
+					if (record.RecordGuid != guid)
+						modified.Add(record);
+				}
 
-            try
-            {
-                string regJson = JsonConvert.SerializeObject(modified);
-                File.WriteAllText(StorePath, regJson);
-            }
-            catch (IOException)
-            {
-                return false;
-            }
-
-            return true;
+				try
+				{
+					string regJson = JsonConvert.SerializeObject(modified);
+					File.WriteAllText(StorePath, regJson);
+				}
+				catch (IOException)
+				{
+					return false;
+				}
+				return true;
+			}
 		}
 
-        public bool Delete(RegistrationRecord record)
-        {
-            try
-            {
-                Delete(record.RecordGuid);
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        private List<RegistrationRecord> GetRecordsOrCreateFile()
-        {
-			List<RegistrationRecord> registrations = new List<RegistrationRecord>();
-
+		public bool Delete(RegistrationRecord record)
+		{
 			try
 			{
-				registrations = JsonConvert.DeserializeObject<List<RegistrationRecord>>(File.ReadAllText(StorePath));
-
-				// If file is empty the list will deserialise to null
-				if (registrations == null)
-					registrations = new List<RegistrationRecord>();
+				Delete(record.RecordGuid);
 			}
-			catch (FileNotFoundException)
+			catch (Exception)
 			{
-				FileStream temp = File.Create(StorePath);
-				temp.Dispose();
+				return false;
 			}
 
-            return registrations;
-        }
+			return true;
+		}
 
-        private string GetRegistrationStoreFilePath(Network network)
-        {
-            string defaultFolderPath;
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                defaultFolderPath = $@"{Environment.GetEnvironmentVariable("AppData")}\StratisNode";
-            }
-            else
-            {
-                defaultFolderPath = $"{Environment.GetEnvironmentVariable("HOME")}/.stratisnode";
-            }
+		private List<RegistrationRecord> GetRecordsOrCreateFile()
+		{
+			lock (RegistrationStore.lock_object)
+			{
 
-            // Create the directory if it doesn't exist
-            Directory.CreateDirectory(defaultFolderPath);
-            return Path.Combine(defaultFolderPath, network.Name, StoreFileName);
-        }
-    }
+				List<RegistrationRecord> registrations = new List<RegistrationRecord>();
+
+				try
+				{
+					registrations = JsonConvert.DeserializeObject<List<RegistrationRecord>>(File.ReadAllText(StorePath));
+
+					// If file is empty the list will deserialise to null
+					if (registrations == null)
+						registrations = new List<RegistrationRecord>();
+				}
+				catch (FileNotFoundException)
+				{
+					FileStream temp = File.Create(StorePath);
+					temp.Dispose();
+				}
+				return registrations;
+			}
+		}
+
+		private string GetRegistrationStoreFilePath(Network network)
+		{
+			string defaultFolderPath;
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				defaultFolderPath = $@"{Environment.GetEnvironmentVariable("AppData")}\StratisNode";
+			}
+			else
+			{
+				defaultFolderPath = $"{Environment.GetEnvironmentVariable("HOME")}/.stratisnode";
+			}
+
+			// Create the directory if it doesn't exist
+			Directory.CreateDirectory(defaultFolderPath);
+			return Path.Combine(defaultFolderPath, network.Name, StoreFileName);
+		}
+	}
 }
