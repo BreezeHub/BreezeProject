84e678b7f7c72619469a0bbab9eb2741158178c2
zeptin <zeptin@gmail.com>
2017-10-08 20:19:06 +0200

    Temporarily use NStratis source instead of NuGet

--- Breeze.BreezeServer/Services/TumblerService.cs
+++ Breeze.BreezeServer/Services/TumblerService.cs
@@ -7,8 +7,6 @@ using Microsoft.Extensions.Logging;
 
 using NTumbleBit;
 using NTumbleBit.Logging;
-using NTumbleBit.ClassicTumbler;
-using NTumbleBit.ClassicTumbler.Server.CLI;
 using NTumbleBit.ClassicTumbler.Server;
 using NTumbleBit.Services;
 using NTumbleBit.Configuration;
@@ -26,53 +24,52 @@ namespace Breeze.BreezeServer
 				// TODO: Tumbler is locked to testnet for testing
 				args = new string[] {"-testnet"};
 			else
-				args = new string[] {"-testnet"};
+				args = new string[] {"-regtest"};
 
-            ConsoleLoggerProcessor loggerProcessor = new ConsoleLoggerProcessor();
-            Logs.Configure(new FuncLoggerFactory(i => new CustomerConsoleLogger(i, Logs.SupportDebug(false), false, loggerProcessor)));
+            Logs.Configure(new FuncLoggerFactory(i => new CustomerConsoleLogger(i, (a, b) => true, false)));
 
             using (var interactive = new Interactive())
-			{
-				var config = new TumblerConfiguration();
-				config.LoadArgs(args);
-				try
-				{
-					var runtime = TumblerRuntime.FromConfiguration(config, new TextWriterClientInteraction(Console.Out, Console.In));
-					interactive.Runtime = new ServerInteractiveRuntime(runtime);
-					StoppableWebHost host = null;
-					if (!config.OnlyMonitor)
-					{
-						host = new StoppableWebHost(() => new WebHostBuilder()
-						.UseAppConfiguration(runtime)
-						.UseContentRoot(Directory.GetCurrentDirectory())
-						.UseStartup<Startup>()
-						.Build());
-					}
+            {
+                var config = new TumblerConfiguration();
+                config.LoadArgs(args);
+                try
+                {
+                    var runtime = TumblerRuntime.FromConfiguration(config, new TextWriterClientInteraction(Console.Out, Console.In));
+                    interactive.Runtime = new ServerInteractiveRuntime(runtime);
+                    StoppableWebHost host = null;
+                    if (!config.OnlyMonitor)
+                    {
+                        host = new StoppableWebHost(() => new WebHostBuilder()
+                        .UseAppConfiguration(runtime)
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseStartup<Startup>()
+                        .Build());
+                    }
 
-					var job = new BroadcasterJob(interactive.Runtime.Services);
-					job.Start();
-					interactive.Services.Add(job);
+                    var job = new BroadcasterJob(interactive.Runtime.Services);
+                    job.Start();
+                    interactive.Services.Add(job);
 
-					if (!config.OnlyMonitor)
-					{
-						host.Start();
-						interactive.Services.Add(host);
-					}
+                    if (!config.OnlyMonitor)
+                    {
+                        host.Start();
+                        interactive.Services.Add(host);
+                    }
 
-					interactive.StartInteractive();
-				}
-				catch (ConfigException ex)
-				{
-					if (!string.IsNullOrEmpty(ex.Message))
-						Logs.Configuration.LogError(ex.Message);
-				}
-				catch (InterruptedConsoleException) { }
-				catch (Exception exception)
-				{
-					Logs.Tumbler.LogError("Exception thrown while running the server");
-					Logs.Tumbler.LogError(exception.ToString());
-				}
-			}
-		}
+                    interactive.StartInteractive();
+                }
+                catch (ConfigException ex)
+                {
+                    if (!string.IsNullOrEmpty(ex.Message))
+                        Logs.Configuration.LogError(ex.Message);
+                }
+                catch (InterruptedConsoleException) { }
+                catch (Exception exception)
+                {
+                    Logs.Tumbler.LogError("Exception thrown while running the server");
+                    Logs.Tumbler.LogError(exception.ToString());
+                }
+            }
+        }
 	}
 }
