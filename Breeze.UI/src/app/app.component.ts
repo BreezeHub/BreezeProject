8862ce982b1edd8a8ccc45822e8f4b94554c6f8f
dev0tion <dev0tion@hotmail.com>
2017-10-26 22:58:28 +0200

    Privacy is in alpha

--- Breeze.UI/src/app/app.component.ts
+++ Breeze.UI/src/app/app.component.ts
@@ -38,7 +38,7 @@ export class AppComponent implements OnInit {
   private setTitle() {
     let applicationName = "Breeze Wallet";
     let applicationVersion = remote.app.getVersion();
-    let releaseCycle = "beta";
+    let releaseCycle = "alpha";
     let newTitle = applicationName + " v" + applicationVersion + " " + releaseCycle;
     this.titleService.setTitle(newTitle);
   }
