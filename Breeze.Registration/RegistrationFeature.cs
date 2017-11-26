34390f7aca1d8e64c6c545b8e0408fdb9758a680
Kevin Loubser <zeptin@gmail.com>
2017-11-26 15:10:39 +0200

    WIP syncing registrations from optimal block height

--- Breeze.Registration/RegistrationFeature.cs
+++ Breeze.Registration/RegistrationFeature.cs
@@ -12,6 +12,7 @@ using Stratis.Bitcoin.Utilities;
 using BreezeCommon;
 using Stratis.Bitcoin.Signals;
 using Stratis.Bitcoin.Features.WatchOnlyWallet;
+using Stratis.Bitcoin.Features.Notifications.Interfaces;
 
 namespace Breeze.Registration
 {
@@ -23,6 +24,7 @@ namespace Breeze.Registration
         private readonly ConcurrentChain chain;
         private readonly Signals signals;
         private IWatchOnlyWalletManager watchOnlyWalletManager;
+	    private readonly IBlockNotification blockNotification;
 
         private ILoggerFactory loggerFactory;
         private readonly IRegistrationManager registrationManager;
@@ -32,7 +34,14 @@ namespace Breeze.Registration
         private bool isBitcoin;
         private Network network;
 
-        public RegistrationFeature(ILoggerFactory loggerFactory, NodeSettings nodeSettings, RegistrationManager registrationManager, RegistrationStore registrationStore, ConcurrentChain chain, Signals signals, IWatchOnlyWalletManager watchOnlyWalletManager)
+        public RegistrationFeature(ILoggerFactory loggerFactory,
+            NodeSettings nodeSettings,
+            RegistrationManager registrationManager,
+            RegistrationStore registrationStore,
+            ConcurrentChain chain,
+            Signals signals,
+            IWatchOnlyWalletManager watchOnlyWalletManager,
+            IBlockNotification blockNotification)
 		{
             this.loggerFactory = loggerFactory;
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
@@ -43,6 +52,7 @@ namespace Breeze.Registration
             this.signals = signals;
             this.network = nodeSettings.Network;
             this.watchOnlyWalletManager = watchOnlyWalletManager;
+		    this.blockNotification = blockNotification;
 
             if (nodeSettings.Network == Network.Main || nodeSettings.Network == Network.TestNet || nodeSettings.Network == Network.RegTest)
             {
@@ -63,6 +73,19 @@ namespace Breeze.Registration
 		{
             if (!this.isBitcoin)
             {
+                // Start syncing from the height of the earliest possible registration transaction instead of the genesis block. This saves time.
+                //this.logger.LogDebug("Registration: blockNotification.StartHash = " + this.blockNotification.StartHash);
+                if (true) // (this.blockNotification.StartHash == null)
+                {
+                    if (this.network == Network.StratisMain) // 616966
+                        this.blockNotification.SyncFrom(uint256.Parse("d88a5b3734b991eca919b399cc676b4990f93a7b1b59aba0d42b13ffe7ec1169"));
+
+                    if (this.network == Network.StratisTest) // 168994, first registration was in 168995
+                        this.blockNotification.SyncFrom(uint256.Parse("120f5aab8a3b82ca273d4f3c5a8ae698d1d4135014ad0d813a16f9272c5dca58"));
+
+                    // For regtest, it is not clear that re-issuing a sync command will be beneficial. Generally you want to sync from genesis in that case.
+                }
+
                 // Only need to subscribe to receive blocks and transactions on the Stratis network
                 this.blockSubscriberdDisposable = this.signals.SubscribeForBlocks(new RegistrationBlockObserver(this.chain, this.registrationManager));
                 //this.transactionSubscriberdDisposable = this.signals.SubscribeForTransactions(new TransactionObserver(this.registrationManager));
