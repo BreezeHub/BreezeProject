8673ba3c8ca8988249690b2e33c77e34f6e7c9f4
zeptin <zeptin@gmail.com>
2017-12-28 16:11:25 +0200

    Suppress batch delays on regtest

--- NTumbleBit/Services/ExternalServices.cs
+++ NTumbleBit/Services/ExternalServices.cs
@@ -33,24 +33,54 @@ namespace NTumbleBit.Services
 			var cache = new RPCWalletCache(rpc, repository);
 
 			var clientBatchInterval = TimeSpan.FromMilliseconds(100);
-			service.WalletService = new RPCWalletService(rpc)
+			if (rpc.Network != NBitcoin.Network.RegTest)
 			{
-				BatchInterval = useBatching ? TimeSpan.FromSeconds(160) : clientBatchInterval,
-				AddressGenerationBatchInterval = useBatching ? TimeSpan.FromSeconds(1) : TimeSpan.FromMilliseconds(10)
-			};
-			service.BroadcastService = new RPCBroadcastService(rpc, cache, repository)
+				service.WalletService = new RPCWalletService(rpc)
+				{
+					BatchInterval = useBatching ? TimeSpan.FromSeconds(160) : clientBatchInterval,
+					AddressGenerationBatchInterval = useBatching ? TimeSpan.FromSeconds(1) : TimeSpan.FromMilliseconds(10)
+				};
+			
+				service.BroadcastService = new RPCBroadcastService(rpc, cache, repository)
+				{
+					BatchInterval = useBatching ? TimeSpan.FromSeconds(5) : clientBatchInterval
+				};
+				service.BlockExplorerService = new RPCBlockExplorerService(rpc, cache, repository)
+				{
+					BatchInterval = useBatching ? TimeSpan.FromSeconds(5) : clientBatchInterval
+				};
+				service.TrustedBroadcastService = new RPCTrustedBroadcastService(rpc, service.BroadcastService, service.BlockExplorerService, repository, cache, tracker)
+				{
+					//BlockExplorer will already track the addresses, since they used a shared bitcoind, no need of tracking again (this would overwrite labels)
+					TrackPreviousScriptPubKey = false
+				};
+			}
+			else
 			{
-				BatchInterval = useBatching ? TimeSpan.FromSeconds(5) : clientBatchInterval
-			};
-			service.BlockExplorerService = new RPCBlockExplorerService(rpc, cache, repository)
-			{
-				BatchInterval = useBatching ? TimeSpan.FromSeconds(5) : clientBatchInterval
-			};
-			service.TrustedBroadcastService = new RPCTrustedBroadcastService(rpc, service.BroadcastService, service.BlockExplorerService, repository, cache, tracker)
-			{
-				//BlockExplorer will already track the addresses, since they used a shared bitcoind, no need of tracking again (this would overwrite labels)
-				TrackPreviousScriptPubKey = false
-			};
+				// For integration tests on regtest the batching needs to be almost nonexistent due to the low
+				// inter-block delays
+				
+				service.WalletService = new RPCWalletService(rpc)
+				{
+					BatchInterval = useBatching ? TimeSpan.FromSeconds(1) : clientBatchInterval,
+					AddressGenerationBatchInterval = useBatching ? TimeSpan.FromSeconds(1) : TimeSpan.FromMilliseconds(10)
+				};
+			
+				service.BroadcastService = new RPCBroadcastService(rpc, cache, repository)
+				{
+					BatchInterval = useBatching ? TimeSpan.FromSeconds(1) : clientBatchInterval
+				};
+				service.BlockExplorerService = new RPCBlockExplorerService(rpc, cache, repository)
+				{
+					BatchInterval = useBatching ? TimeSpan.FromSeconds(1) : clientBatchInterval
+				};
+				service.TrustedBroadcastService = new RPCTrustedBroadcastService(rpc, service.BroadcastService, service.BlockExplorerService, repository, cache, tracker)
+				{
+					//BlockExplorer will already track the addresses, since they used a shared bitcoind, no need of tracking again (this would overwrite labels)
+					TrackPreviousScriptPubKey = false
+				};				
+			}
+			
 			return service;
 		}
 		public IFeeService FeeService
