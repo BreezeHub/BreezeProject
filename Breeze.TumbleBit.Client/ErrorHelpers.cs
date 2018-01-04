5e83368fa391d3a8e642f911f2976ac66b868d20
Igor Goldobin <igor.goldobin@gmail.com>
2018-01-05 00:00:22 +1000

    Added enhanced error model with developer message and help url

---
+++ Breeze.TumbleBit.Client/ErrorHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Breeze.TumbleBit.Client.Models;
+
+namespace Breeze.TumbleBit.Client
+{
+    public static class ErrorHelpers
+    {
+        public static ErrorResult BuildErrorResponse(
+            HttpStatusCode statusCode,
+            string message,
+            string description,
+            Exception ex = null,
+            string additionalInfoUrl = null)
+        {
+            var errorResponse = new ErrorResponse
+            {
+                Errors = new List<ErrorModel>
+                {
+                    ErrorModel.Create(statusCode, message, description, ex, additionalInfoUrl)
+                }
+            };
+
+            return new ErrorResult((int)statusCode, errorResponse);
+        }
+    }
+}
---
+++ Breeze.TumbleBit.Client/Models/ErrorModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Breeze.TumbleBit.Client.Models
+{
+    public class ErrorModel : Stratis.Bitcoin.Utilities.JsonErrors.ErrorModel
+    {
+        public ErrorModel() { }
+
+        private ErrorModel(
+            HttpStatusCode code,
+            string message,
+            string description,
+            Exception ex = null,
+            string additionalInfoUrl = null)
+        {
+            this.Status = (int)code;
+            if (ex != null) this.DeveloperMessage = ex.Message;
+            this.Message = message;
+            this.Description = description;
+            if (!string.IsNullOrEmpty(additionalInfoUrl)) this.AdditionalInfoUrl = additionalInfoUrl;
+        }
+
+        public static ErrorModel Create(
+            HttpStatusCode code,
+            string message,
+            string description,
+            Exception ex = null,
+            string additionalInfoUrl = null)
+        {
+            return new ErrorModel(code, message, description, ex, additionalInfoUrl);
+        }
+
+        [JsonProperty(PropertyName = "additionalInfoUrl")]
+        public string AdditionalInfoUrl { get; set; }
+
+        [JsonProperty(PropertyName = "developerMessage")]
+        public string DeveloperMessage { get; set; }
+    }
+}
