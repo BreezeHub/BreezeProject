a089f443292984407cc80d195c4368562d650e67
Igor Goldobin <igor.goldobin@gmail.com>
2018-02-26 21:04:10 +1000

    Updated globas service port resolution

--- Breeze.UI/src/app/shared/services/global.service.ts
+++ Breeze.UI/src/app/shared/services/global.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class GlobalService {
+  private currentBitcoinApiPort = 37220;
+  private currentStratisApiPort = 37221;
   private walletPath: string;
   private currentWalletName: string;
   private coinType: number;
@@ -12,19 +14,11 @@ export class GlobalService {
   constructor() {}
 
   get bitcoinApiPort() {
-    if (this.network.toLowerCase() === 'testnet') {
-      return 38220;
-    } else {
-      return 37220;
-    }
+    return this.currentBitcoinApiPort;
   }
 
   get stratisApiPort() {
-    if (this.network.toLowerCase() === 'testnet') {
-      return 38221;
-    } else {
-      return 37221;
-    }
+    return this.currentStratisApiPort;
   }
 
   getWalletPath() {
@@ -41,6 +35,13 @@ export class GlobalService {
 
   setNetwork(network: string) {
     this.network = network;
+    if (!!network && network.toLowerCase() === 'testnet') {
+      this.currentBitcoinApiPort = 38220;
+      this.currentStratisApiPort = 38221;
+    } else {
+      this.currentBitcoinApiPort = 37220;
+      this.currentStratisApiPort = 37221;
+    }
   }
 
   getWalletName() {
