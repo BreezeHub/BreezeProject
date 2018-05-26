f19cdf1b289aff746a8a914f709794e887609211
Kevin Loubser <zeptin@gmail.com>
2018-05-26 16:32:24 +0200

    Fix tab issues

--- NTumbleBit/ClassicTumbler/Client/StateMachinesExecutor.cs
+++ NTumbleBit/ClassicTumbler/Client/StateMachinesExecutor.cs
@@ -10,73 +10,73 @@ using System.Threading.Tasks;
 
 namespace NTumbleBit.ClassicTumbler.Client
 {
-	public class StateMachinesExecutor : TumblerServiceBase
-	{
-		public StateMachinesExecutor(
-			TumblerClientRuntime runtime)
-		{
+    public class StateMachinesExecutor : TumblerServiceBase
+    {
+        public StateMachinesExecutor(
+            TumblerClientRuntime runtime)
+        {
             Runtime = runtime ?? throw new ArgumentNullException("runtime");
-			_ParametersHash = Runtime.TumblerParameters.GetHash();
-		}
+            _ParametersHash = Runtime.TumblerParameters.GetHash();
+        }
 
-		uint160 _ParametersHash;
-		public TumblerClientRuntime Runtime
-		{
-			get; set;
-		}
+        uint160 _ParametersHash;
+        public TumblerClientRuntime Runtime
+        {
+            get; set;
+        }
 
-		public override string Name => "mixer";
+        public override string Name => "mixer";
 
-		public int InvalidPhaseCount
-		{
-			get;
-			private set;
-		}
+        public int InvalidPhaseCount
+        {
+            get;
+            private set;
+        }
 
-		protected override void StartCore(CancellationToken cancellationToken)
-		{
-			new Thread(() =>
-			{
-				Logs.Client.LogInformation("State machines started");
-				uint256 lastBlock = uint256.Zero;
-				int lastCycle = 0;
-				while(true)
-				{
-					try
-					{
-						lastBlock = Runtime.Services.BlockExplorerService.WaitBlock(lastBlock, cancellationToken);
-						var height = Runtime.Services.BlockExplorerService.GetCurrentHeight();
-						Logs.Client.LogInformation("New Block: " + height);
-						var cycle = Runtime.TumblerParameters.CycleGenerator.GetRegisteringCycle(height);
-						if(lastCycle != cycle.Start)
-						{
-							// Only start a new cycle if there are sufficient wallet funds
-							Money walletBalance = this.Runtime.Services.WalletService.GetBalance();
-							Money minimumBalance = this.Runtime.TumblerParameters.Denomination + this.Runtime.TumblerParameters.Fee;
+        protected override void StartCore(CancellationToken cancellationToken)
+        {
+            new Thread(() =>
+            {
+                Logs.Client.LogInformation("State machines started");
+                uint256 lastBlock = uint256.Zero;
+                int lastCycle = 0;
+                while(true)
+                {
+                    try
+                    {
+                        lastBlock = Runtime.Services.BlockExplorerService.WaitBlock(lastBlock, cancellationToken);
+                        var height = Runtime.Services.BlockExplorerService.GetCurrentHeight();
+                        Logs.Client.LogInformation("New Block: " + height);
+                        var cycle = Runtime.TumblerParameters.CycleGenerator.GetRegisteringCycle(height);
+                        if(lastCycle != cycle.Start)
+                        {
+                            // Only start a new cycle if there are sufficient wallet funds
+                            Money walletBalance = this.Runtime.Services.WalletService.GetBalance();
+                            Money minimumBalance = this.Runtime.TumblerParameters.Denomination + this.Runtime.TumblerParameters.Fee;
 
-							if (walletBalance >= minimumBalance)
-							{
-								lastCycle = cycle.Start;
-								Logs.Client.LogInformation("New Cycle: " + cycle.Start);
-								PaymentStateMachine.State state = GetPaymentStateMachineState(cycle);
-								if (state == null)
-								{
-									var stateMachine = new PaymentStateMachine(Runtime, null);
-									stateMachine.NeedSave = true;
-									Save(stateMachine, cycle.Start);
-								}
-							}
-						}
+                            if (walletBalance >= minimumBalance)
+                            {
+                                lastCycle = cycle.Start;
+                                Logs.Client.LogInformation("New Cycle: " + cycle.Start);
+                                PaymentStateMachine.State state = GetPaymentStateMachineState(cycle);
+                                if (state == null)
+                                {
+                                    var stateMachine = new PaymentStateMachine(Runtime, null);
+                                    stateMachine.NeedSave = true;
+                                    Save(stateMachine, cycle.Start);
+                                }
+                            }
+                        }
 
-						var progressInfo = new ProgressInfo(height);
+                        var progressInfo = new ProgressInfo(height);
 
-						var cycles = Runtime.TumblerParameters.CycleGenerator.GetCycles(height);
-						var machineStates = cycles
-												.SelectMany(c => Runtime.Repository.List<PaymentStateMachine.State>(GetPartitionKey(c.Start)))
-												.Where(m => m.TumblerParametersHash == _ParametersHash)
-												.ToArray();
-						NBitcoin.Utils.Shuffle(machineStates);
-						bool hadInvalidPhase = false;
+                        var cycles = Runtime.TumblerParameters.CycleGenerator.GetCycles(height);
+                        var machineStates = cycles
+                                                .SelectMany(c => Runtime.Repository.List<PaymentStateMachine.State>(GetPartitionKey(c.Start)))
+                                                .Where(m => m.TumblerParametersHash == _ParametersHash)
+                                                .ToArray();
+                        NBitcoin.Utils.Shuffle(machineStates);
+                        bool hadInvalidPhase = false;
 
                         if (Runtime.Network != Network.RegTest)
                         {
@@ -100,88 +100,88 @@ namespace NTumbleBit.ClassicTumbler.Client
                         }
 
                         foreach (var state in machineStates)
-						{
-							var machine = new PaymentStateMachine(Runtime, state);
-							if(machine.Status == PaymentStateMachineStatus.Wasted)
-							{
-								Logs.Client.LogDebug($"Skipping cycle {machine.StartCycle}, because if is wasted");
-								continue;
-							}
+                        {
+                            var machine = new PaymentStateMachine(Runtime, state);
+                            if(machine.Status == PaymentStateMachineStatus.Wasted)
+                            {
+                                Logs.Client.LogDebug($"Skipping cycle {machine.StartCycle}, because if is wasted");
+                                continue;
+                            }
 
-							var statusBefore = machine.GetInternalState();
-							try
-							{
-								var cycleProgressInfo = machine.Update();
-								InvalidPhaseCount = 0;
-								if (cycleProgressInfo != null)
-									progressInfo.CycleProgressInfoList.Add(cycleProgressInfo);
-							}
-							catch(PrematureRequestException)
-							{
-								Logs.Client.LogInformation("Skipping update, need to wait for tor circuit renewal");
-								break;
-							}
-							catch(Exception ex) when(IsInvalidPhase(ex))
-							{
-								if(!hadInvalidPhase)
-								{
-									hadInvalidPhase = true;
-									InvalidPhaseCount++;
-									if(InvalidPhaseCount > 2)
-									{
-										Logs.Client.LogError(new EventId(), ex, $"Invalid-Phase happened repeatedly, check that your node currently at height {height} is currently sync to the network");
-									}
-								}
-							}
-							catch(Exception ex)
-							{
-								Logs.Client.LogError(new EventId(), ex, "Unhandled StateMachine Error");
-							}
+                            var statusBefore = machine.GetInternalState();
+                            try
+                            {
+                                var cycleProgressInfo = machine.Update();
+                                InvalidPhaseCount = 0;
+                                if (cycleProgressInfo != null)
+                                    progressInfo.CycleProgressInfoList.Add(cycleProgressInfo);
+                            }
+                            catch(PrematureRequestException)
+                            {
+                                Logs.Client.LogInformation("Skipping update, need to wait for tor circuit renewal");
+                                break;
+                            }
+                            catch(Exception ex) when(IsInvalidPhase(ex))
+                            {
+                                if(!hadInvalidPhase)
+                                {
+                                    hadInvalidPhase = true;
+                                    InvalidPhaseCount++;
+                                    if(InvalidPhaseCount > 2)
+                                    {
+                                        Logs.Client.LogError(new EventId(), ex, $"Invalid-Phase happened repeatedly, check that your node currently at height {height} is currently sync to the network");
+                                    }
+                                }
+                            }
+                            catch(Exception ex)
+                            {
+                                Logs.Client.LogError(new EventId(), ex, "Unhandled StateMachine Error");
+                            }
 
-							Save(machine);
-						}
+                            Save(machine);
+                        }
 
-						progressInfo.Save();
-					}
-					catch (OperationCanceledException) when(cancellationToken.IsCancellationRequested)
-					{
-						Stopped();
-						break;
-					}
-					catch(Exception ex)
-					{
-						Logs.Client.LogError(new EventId(), ex, "StateMachineExecutor Error: " + ex.ToString());
-						cancellationToken.WaitHandle.WaitOne(5000);
-					}
-				}
+                        progressInfo.Save();
+                    }
+                    catch (OperationCanceledException) when(cancellationToken.IsCancellationRequested)
+                    {
+                        Stopped();
+                        break;
+                    }
+                    catch(Exception ex)
+                    {
+                        Logs.Client.LogError(new EventId(), ex, "StateMachineExecutor Error: " + ex.ToString());
+                        cancellationToken.WaitHandle.WaitOne(5000);
+                    }
+                }
 
-			}).Start();
-		}
+            }).Start();
+        }
 
 
 
-		private bool IsInvalidPhase(Exception ex)
-		{
-			return ex.Message.IndexOf("invalid-phase", StringComparison.OrdinalIgnoreCase) >= 0;
-		}
+        private bool IsInvalidPhase(Exception ex)
+        {
+            return ex.Message.IndexOf("invalid-phase", StringComparison.OrdinalIgnoreCase) >= 0;
+        }
 
-		public PaymentStateMachine.State GetPaymentStateMachineState(CycleParameters cycle)
-		{
-			var state = Runtime.Repository.Get<PaymentStateMachine.State>(GetPartitionKey(cycle.Start), "");
-			if(state == null)
-				return null;
-			return state.TumblerParametersHash == _ParametersHash ? state : null;
-		}
+        public PaymentStateMachine.State GetPaymentStateMachineState(CycleParameters cycle)
+        {
+            var state = Runtime.Repository.Get<PaymentStateMachine.State>(GetPartitionKey(cycle.Start), "");
+            if(state == null)
+                return null;
+            return state.TumblerParametersHash == _ParametersHash ? state : null;
+        }
 
-		private string GetPartitionKey(int cycle)
-		{
-			return "Cycle_" + cycle;
-		}
+        private string GetPartitionKey(int cycle)
+        {
+            return "Cycle_" + cycle;
+        }
 
-		private void Save(PaymentStateMachine stateMachine, int? cycleStart = null)
-		{
-			if(stateMachine.NeedSave)
-				Runtime.Repository.UpdateOrInsert(GetPartitionKey(cycleStart ?? stateMachine.StartCycle), "", stateMachine.GetInternalState(), (o, n) => n);
-		}
-	}
+        private void Save(PaymentStateMachine stateMachine, int? cycleStart = null)
+        {
+            if(stateMachine.NeedSave)
+                Runtime.Repository.UpdateOrInsert(GetPartitionKey(cycleStart ?? stateMachine.StartCycle), "", stateMachine.GetInternalState(), (o, n) => n);
+        }
+    }
 }
--- NTumbleBit/Services/IWalletService.cs
+++ NTumbleBit/Services/IWalletService.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace NTumbleBit.Services
 {
-	public interface IWalletService
+    public interface IWalletService
     {
-		Task<IDestination> GenerateAddressAsync();
-		Task<Transaction> FundTransactionAsync(TxOut txOut, FeeRate feeRate);
-		Task<Transaction> ReceiveAsync(ScriptCoin escrowedCoin, TransactionSignature clientSignature, Key escrowKey, FeeRate feeRate);
-	    Money GetBalance();
+        Task<IDestination> GenerateAddressAsync();
+        Task<Transaction> FundTransactionAsync(TxOut txOut, FeeRate feeRate);
+        Task<Transaction> ReceiveAsync(ScriptCoin escrowedCoin, TransactionSignature clientSignature, Key escrowKey, FeeRate feeRate);
+        Money GetBalance();
     }
 }
