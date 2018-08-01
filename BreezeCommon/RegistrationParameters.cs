552ec7f7c4c94cbe218bfd5a761cec168f64acf2
Paul Herbert <paul.herbert@stratisplatform.com>
2018-08-01 11:15:48 +0100

    Changes in order to confirm legal text.

--- BreezeCommon/RegistrationParameters.cs
+++ BreezeCommon/RegistrationParameters.cs
@@ -7,7 +7,7 @@ namespace BreezeCommon
 {
     public static class RegistrationParameters
     {
-        public static readonly Money MASTERNODE_COLLATERAL_THRESHOLD = new Money(250000, MoneyUnit.BTC);
+        public static readonly Money MASTERNODE_COLLATERAL_THRESHOLD = new Money(5, MoneyUnit.BTC);
         public static readonly int MAX_PROTOCOL_VERSION = 128; // >128 = regard as test versions
         public static readonly int MIN_PROTOCOL_VERSION = 1;
         public static readonly int WINDOW_PERIOD_BLOCK_COUNT = 30;
