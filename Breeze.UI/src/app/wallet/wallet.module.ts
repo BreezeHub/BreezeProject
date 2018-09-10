1e157a4404b568568abdd819a6e2c0993922034d
Paul Herbert <paul.herbert@stratisplatform.com>
2018-09-10 16:44:42 +0100

    Added stratis-tumblebit.component which is now assigned to the Privacy tab for the Stratis wallet.

--- Breeze.UI/src/app/wallet/wallet.module.ts
+++ Breeze.UI/src/app/wallet/wallet.module.ts
@@ -3,18 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ClipboardModule } from 'ngx-clipboard';
+import { SharedModule } from '../shared/shared.module';
+import { WalletRoutingModule } from './wallet-routing.module';
 
 import { WalletComponent } from './wallet.component';
 import { MenuComponent } from './menu/menu.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TumblebitComponent } from './tumblebit/tumblebit.component';
+import { StratisTumblebitComponent } from './tumblebit/stratis-tumblebit.component';
 import { HistoryComponent } from './history/history.component';
-
-import { SharedModule } from '../shared/shared.module';
-import { WalletRoutingModule } from './wallet-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { StatusBarComponent } from './status-bar/status-bar.component';
-import { TransactionDetailsComponent } from './transaction-details/transaction-details.component';
 import { ConnectionProgressComponent } from './tumblebit/connection-progress/connection-progress.component';
 
 @NgModule({
@@ -32,6 +31,7 @@ import { ConnectionProgressComponent } from './tumblebit/connection-progress/con
     MenuComponent,
     DashboardComponent,
     TumblebitComponent,
+    StratisTumblebitComponent,
     HistoryComponent,
     SidebarComponent,
     StatusBarComponent,
