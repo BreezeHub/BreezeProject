59bff8f2f0e3a1b258e15dc82b6819bf421651cf
dev0tion <dev0tion@hotmail.com>
2019-03-27 17:26:51 +0100

    Update models

--- Breeze.UI/src/app/shared/classes/transaction-info.ts
+++ Breeze.UI/src/app/shared/classes/transaction-info.ts
@@ -1,14 +1,11 @@
-import { Transaction } from 'interfaces/transaction'
-
 export class TransactionInfo {
-
-  constructor(transaction: Transaction) {
-    this.transactionType = transaction.type;
-    this.transactionId = transaction.id;
-    this.transactionAmount = transaction.amount;
-    this.transactionFee = transaction.fee || 0;
-    this.transactionConfirmedInBlock = transaction.confirmedInBlock;
-    this.transactionTimestamp = transaction.timestamp;
+  constructor(transactionType: string, transactionId: string, transactionAmount: number, transactionFee: number, transactionConfirmedInBlock: number, transactionTimestamp: number) {
+    this.transactionType = transactionType;
+    this.transactionId = transactionId;
+    this.transactionAmount = transactionAmount;
+    this.transactionFee = transactionFee;
+    this.transactionConfirmedInBlock = transactionConfirmedInBlock;
+    this.transactionTimestamp = transactionTimestamp;
   }
 
   public transactionType: string;
@@ -16,5 +13,5 @@ export class TransactionInfo {
   public transactionAmount: number;
   public transactionFee: number;
   public transactionConfirmedInBlock?: number;
-  public transactionTimestamp: string;
+  public transactionTimestamp: number;
 }
