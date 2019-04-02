b082173663196277568ece3612f2f8dbf67ea103
dev0tion <dev0tion@hotmail.com>
2019-04-02 13:01:25 +0200

    Adjust tumblebit service to new RXJS version

--- Breeze.UI/src/app/wallet/tumblebit/tumblebit.service.ts
+++ Breeze.UI/src/app/wallet/tumblebit/tumblebit.service.ts
@@ -1,16 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions, URLSearchParams } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/concat';
-import 'rxjs/add/observable/throw';
-import { TumblerConnectionRequest } from './classes/tumbler-connection-request';
+import { Observable, of, interval } from 'rxjs';
+import { delay, retryWhen, map, mergeMap, take, concat, startWith, switchMap } from 'rxjs/operators';
 import { TumbleRequest } from './classes/tumble-request';
 import { ConnectRequest } from './classes/connect-request';
 import { GlobalService } from '../../shared/services/global.service';
-import { Error } from '../../shared/classes/error';
 import { ServiceShared } from '../../shared/services/shared';
 
 @Injectable()
@@ -19,7 +13,7 @@ export class TumblebitService {
   // The service to connect to & operate a TumbleBit Server via the
   // TumbleBit.Client.CLI tool
   private headers = new Headers({ 'Content-Type': 'application/json' });
-  private pollingInterval = 3000;
+  private pollingInterval = interval(3000);
   constructor(private http: Http, private globalService: GlobalService) { };
   get tumblerClientUrl() {
     return `http://localhost:${this.globalService.bitcoinApiPort}/api/TumbleBit/`;
@@ -29,79 +23,75 @@ export class TumblebitService {
   connectToTumbler(operation: 'connect' | 'changeserver', body: ConnectRequest): Observable<any> {
     return this.http
       .post(`${this.tumblerClientUrl}${operation}`, JSON.stringify(body), {headers: this.headers})
-      .retryWhen(e => {
+      .pipe(retryWhen(e => {
         return e
-           .mergeMap((error: any) => {
+           .pipe(mergeMap((error: any) => {
               if (error.status  === 0) {
-                return Observable.of(error.status).delay(5000)
+                return of(error.status).pipe(delay(5000))
               }
               return Observable.throw(error);
-           })
-           .take(5)
-           .concat(Observable.throw(e));
-      })
-      .map((response: Response) => response);
+           }),
+           take(5),
+           concat(Observable.throw(e)))
+      }),
+      map((response: Response) => response));
   };
 
   changeTumblerServer(body: ConnectRequest): Observable<any> {
     return this.http
       .post(`${this.tumblerClientUrl}changeserver`, JSON.stringify(body), {headers: this.headers})
-      .retryWhen(e => {
-        return e
-           .mergeMap((error: any) => {
+      .pipe(retryWhen(e => {
+        return e.pipe(
+           mergeMap((error: any) => {
               if (error.status  === 0) {
-                return Observable.of(error.status).delay(5000)
+                return of(error.status).pipe(delay(5000))
               }
               return Observable.throw(error);
-           })
-           .take(5)
-           .concat(Observable.throw(e));
-      })
-      .map((response: Response) => response);
+           }),
+           take(5),
+           concat(Observable.throw(e)));
+      }),
+      map((response: Response) => response));
   };
 
   getTumblingState(): Observable<any> {
-    return Observable
-      .interval(1000)
-      .startWith(0)
-      .switchMap(
+    return this.pollingInterval.pipe(
+      startWith(0),
+      switchMap(
         () => this.http.get(`${this.tumblerClientUrl}tumbling-state`)
-                      .retryWhen(e => ServiceShared.onRetryWhen(e)))
-      .map((response: Response) => response);
+                      .pipe(retryWhen(e => ServiceShared.onRetryWhen(e)))),
+      map((response: Response) => response));
   }
-  
+
   startTumbling(body: TumbleRequest): Observable<any> {
     return this.http
       .post(`${this.tumblerClientUrl}tumble`, JSON.stringify(body), {headers: this.headers})
-      .map((response: Response) => response);
+      .pipe(map((response: Response) => response));
   };
-  
+
   stopTumbling(): Observable<any> {
     return this.http
       .get(`${this.tumblerClientUrl}onlymonitor`)
-      .map((response: Response) => response);
+      .pipe(map((response: Response) => response));
   }
-  
+
   getProgress(): Observable<any> {
-    return Observable
-      .interval(this.pollingInterval)
-      .startWith(0)
-      .switchMap(
+    return this.pollingInterval.pipe(
+      startWith(0),
+      switchMap(
         () => this.http.get(`${this.tumblerClientUrl}progress`)
-                  .retryWhen(e => ServiceShared.onRetryWhen(e)))
-      .map((response: Response) => response);
+                  .pipe(retryWhen(e => ServiceShared.onRetryWhen(e)))),
+      (map((response: Response) => response)));
   }
 
   getWalletDestinationBalance(data: string): Observable<any> {
     const params: URLSearchParams = new URLSearchParams();
     params.set('walletName', data);
-    return Observable
-      .interval(this.pollingInterval)
-      .startWith(0)
-      .switchMap(
+    return this.pollingInterval.pipe(
+      startWith(0),
+      switchMap(
         () => this.http.get(`${this.tumblerClientUrl}destination-balance`, new RequestOptions({headers: this.headers, search: params}))
-                        .retryWhen(e => ServiceShared.onRetryWhen(e)))
-      .map((response: Response) => response);
+                        .pipe(retryWhen(e => ServiceShared.onRetryWhen(e)))),
+      map((response: Response) => response));
   }
-
 }
